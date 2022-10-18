d2564fdc420a91df9898ae02ac7e6bf75bb8338c
Sean Li <sean.4736zg@gmail.com>
2022-10-18 18:29:12 +0300

    Add urlSession

--- Sources/Tyrant/Tyrant.swift
+++ Sources/Tyrant/Tyrant.swift
@@ -1,5 +1,9 @@
+import Combine
+import Foundation
+
 public struct Tyrant {
     public private(set) var text = "Hello, World!"
+    public private(set) var urlSesstion: URLSession = URLSession.shared
 
     public init() {
     }
