e06c1ac72e70830698738be68e1c46732dc2ab76
Sean Li <sean.4736zg@gmail.com>
2023-01-02 16:52:23 +0200

    Drawing to a UIGraphicsImageRenderer to resize image

--- Sources/Tyrant/Utilities.swift
+++ Sources/Tyrant/Utilities.swift
@@ -7,7 +7,6 @@
 
 import Foundation
 import UIKit
-import Accelerate
 
 /// Easily throw generic errors with a text description.
 extension String: LocalizedError {
@@ -27,39 +26,12 @@ extension Task where Success == Never, Failure == Never {
 struct ResizeError: Error { }
 
 func resize(_ data: Data, to size: CGSize) throws -> UIImage {
-  guard let cgImage = UIImage(data: data)?.cgImage,
-        let colorSpace = cgImage.colorSpace else {
+  guard let image = UIImage(data: data) else {
     throw ResizeError()
   }
   
-  var format = vImage_CGImageFormat(
-    bitsPerComponent: UInt32(cgImage.bitsPerComponent),
-    bitsPerPixel: UInt32(cgImage.bitsPerPixel),
-    colorSpace: Unmanaged.passRetained(colorSpace),
-    bitmapInfo: cgImage.bitmapInfo,
-    version: 0,
-    decode: nil,
-    renderingIntent: cgImage.renderingIntent
-  )
-  
-  var buffer = vImage_Buffer()
-  vImageBuffer_InitWithCGImage(&buffer, &format, nil, cgImage, vImage_Flags(kvImageNoFlags))
-  
-  var destinationBuffer = try vImage_Buffer(width: Int(size.width), height: Int(size.height), bitsPerPixel: format.bitsPerPixel)
-  
-  defer { destinationBuffer.free() }
-  
-  _ = withUnsafePointer(to: buffer) { sourcePointer in
-    vImageScale_ARGB8888(sourcePointer, &destinationBuffer, nil, vImage_Flags(kvImageNoFlags))
+  let renderer = UIGraphicsImageRenderer(size: size)
+  return renderer.image { (context) in
+    image.draw(in: CGRect(origin: .zero, size: size))
   }
-  
-  let destinationCGImage = vImageCreateCGImageFromBuffer(
-    &buffer, &format, nil, nil, vImage_Flags(kvImageNoFlags), nil
-  )
-  
-  guard let destinationCGImage = destinationCGImage else {
-    throw ResizeError()
-  }
-  
-  return UIImage(cgImage: destinationCGImage.takeRetainedValue())
 }
